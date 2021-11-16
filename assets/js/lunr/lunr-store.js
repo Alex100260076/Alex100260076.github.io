03a2c209ab80d1c649937612b49f493d207b8204
Alex100260076 <alu0100260076@ull.edu.es>
2021-11-16 19:13:55 +0000

    entrando en despliegue

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,13 @@
+var store = [{
+        "title": "Aprendiendo a usar un generador estático",
+        "excerpt":"I am an amazing person. por fin es jueves al menos el proyector funciona control shift p es la ayuda de buscar no entiendo porque no se muestran los 3 puntos control s para guardar, pero no va \\[a^2 x\\times\\] \\[f(x) = \\int_{-\\infty}^\\infty \\hat f(\\xi)\\,e^{2 \\pi i \\xi x} \\,d\\xi\\]...","categories": ["docencia"],
+        "tags": [],
+        "url": "/docencia/AET",
+        "teaser": null
+      },{
+        "title": "Aet",
+        "excerpt":"martes y no es de carnaval 4 Me gusta mucho comer naranjas Me gusta mucho comer peras Me gusta mucho comer limones Me gusta mucho comer fresas First name Last name Age Location John Doe 35 United States Jane Doe 29 France Jack Hill 25 Australia La respuesta a cuanto...","categories": ["blog"],
+        "tags": [],
+        "url": "/blog/aet",
+        "teaser": null
+      }]
